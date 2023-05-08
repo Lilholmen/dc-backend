f27cd429f600908a2bbd882968c36216b46055a1
Artur Yandulsky <artur.yandulsky@jetruby.com>
2023-05-08 18:46:12 +0300

    server setup

---
+++ Server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const cors = require('cors')
+
+const app = express()
+
+const PORT = process.env.PORT | 5000
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => res.send('sending'))
+
+app.listen(PORT, () => console.log(`Started at port: ${PORT}`))
